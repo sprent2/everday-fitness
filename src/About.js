b9fb3eb700f0f06a2b651aca3d5243f511598f5f
Amy Prentice <amyprentice@Amys-MBP.fios-router.home>
2018-11-28 20:33:12 -0500

    first commit

---
+++ src/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () =>{
+    return (
+    <header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong>We Help You Live Your Best Life</strong>
+            </h1>
+            <hr/>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5">We will work with you to build a fitness plan that is catered to your lifestyle and interests. We will include a nutrition plan along with the fitness plan to ensure your health success</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+          </div>
+        </div>
+      </div>
+    </header>
+    )
+}
+
+export default About;
