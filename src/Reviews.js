d4de81b88b0a7f98c912db976820ead4a57079a3
Amy Prentice <amyprentice@Amys-MBP.fios-router.home>
2018-12-01 12:03:08 -0500

    added tweets

--- src/Reviews.js
+++ src/Reviews.js
@@ -5,42 +5,42 @@ import TweetEmbed from 'react-tweet-embed'
 const reviews= () => {
     return(
 
-        <section class="p-0" id="portfolio">
-      <div class="container-fluid p-0">
-        <div class="row no-gutters popup-gallery">
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
-            <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }}/>
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
-            <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }} />
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
-            <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }} />
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
-            <TweetEmbed id='692527862369357824'  options={{cards: 'hidden' }}/>
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
-            <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }} />
-            </a>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
-            <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }} />
-            </a>
-          </div>
+        <section class="p-0" id="reviews">
+        <div class="container-fluid p-0">
+            <div class="row no-gutters popup-gallery">
+            <div class="col-lg-4 col-sm-6">
+                <a class="reviews-box" href="img/reviews/fullsize/1.jpg">
+                <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }}/>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="reviews-box" href="img/reviews/fullsize/2.jpg">
+                <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }} />
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="reviews-box" href="img/reviews/fullsize/3.jpg">
+                <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }} />
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="reviews-box" href="img/reviews/fullsize/4.jpg">
+                <TweetEmbed id='692527862369357824'  options={{cards: 'hidden' }}/>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="reviews-box" href="img/reviews/fullsize/5.jpg">
+                <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }} />
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="reviews-box" href="img/reviews/fullsize/6.jpg">
+                <TweetEmbed id='692527862369357824' options={{cards: 'hidden' }} />
+                </a>
+            </div>
+            </div>
         </div>
-      </div>
-    </section>
+        </section>
 
     )
 }
