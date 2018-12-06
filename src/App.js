9492c2a0e9142a9b41d3607c0ceb9aa3e6ba7c4c
Amy Prentice <amyprentice@Amys-MBP.fios-router.home>
2018-12-05 20:04:20 -0500

    Moved the landing page components into a separate folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 //import 'vendor/fontawesome-free/css/all.min.css'
-import Navbar from './Navbar.js'
-import About from './About.js'
-import Description from './Description.js'
+import Navbar from './landingPageComponents/Navbar.js'
+import About from './landingPageComponents/About.js'
+import Description from './landingPageComponents/Description.js'
 import Services from './Services.js'
-import Reviews from './Reviews.js'
-import Contact from './Contact.js'
+import Reviews from './landingPageComponents/Reviews.js'
+import Contact from './landingPageComponents/Contact.js'
 import './Main.css'
 
 class App extends Component {
