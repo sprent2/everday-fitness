769df125e49fd81e3814e22d4a0a0b1621f9f282
Amy Prentice <amyprentice@Amys-MBP.fios-router.home>
2018-12-20 19:37:33 -0500

    added google analytics and functionality to push to s3

--- src/formComponents/Form.js
+++ src/formComponents/Form.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
 import Description from './Description.js';
+import FormContent from './FormContent'
 
 class Form extends React.Component{
     render(){
         return(
             <div>
                 <Description/>
+                <FormContent/>
             </div>
         )
     }
---
+++ src/formComponents/FormContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const FormContent = () => {
+    return (
+        <div>hey </div>
+    )
+}
+export default FormContent;
+
+
